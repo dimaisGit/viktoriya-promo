9c454d460f6250af18643da76402380fa4140d79
Александра Чернышева <ach@worldfashion.pro>
2019-11-21 13:00:26 +0300

    fix

--- src/components/RaffleComponent.js
+++ src/components/RaffleComponent.js
@@ -7,8 +7,20 @@ export default class RaffleComponent extends React.Component {
         return (
             <section className="raffle oranged">
                 <div className="container">
-                    <div className="content-raffle">
-                        <img src={mandarinRaffle} alt="mandarin-raffle" srcSet=""/>
+                    <div className="content-raffle flexing">
+                        {/* <img src={mandarinRaffle} alt="mandarin-raffle" srcSet=""/> */}
+                        <div className="orange-animated">
+                            <div className="tail"></div>
+                            <div className="leave"></div>
+                            <div class="mandarinMain"> </div>
+                            <div className="leftHand"></div>
+                            <div className="rightHand"></div>
+                            <div className="leftBrow"></div>
+                            <div className="rightBrow"></div>
+                            <div className="leftEye"></div>
+                            <div className="rightEye"></div>
+                            <div className="mouth"></div>
+                        </div>
                             <div className="info">
                                 <h2>Розыгрыш</h2>
                                 <p>Розыгрыш призов вы сможете увидеть в прямом эфире тогда-то</p>
