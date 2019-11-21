ac314f55fe3f9de7912d1577d9daa0150ff08188
Александра Чернышева <ach@worldfashion.pro>
2019-11-21 14:24:17 +0300

    fixed sec block

--- src/components/Mechanics.js
+++ src/components/Mechanics.js
@@ -7,8 +7,7 @@ export default class MechanicsComponent extends React.Component {
                 <div className="container">
                     <div className="blocks flexing">
                         <div className="block first">
-                            <p>С <b>16.12.2019</b> ПО <b>31.01.2020</b>
-                                СОВЕРШИТЕ ПОКУПКУ
+                            <p>С <b>16.12.2019</b> ПО <b>31.01.2020</b> СОВЕРШИТЕ ПОКУПКУ
                                 НА СУММУ ОТ 700Р*</p>
                         </div>
                         <div className="block second">
@@ -24,12 +23,13 @@ export default class MechanicsComponent extends React.Component {
                                 от 700 руб.</p>
                         </div>
                         <div className="block no-bg">
-                            <h4>«2020»</h4>
+                            <h4>код «2020»</h4>
                             <p>с шансом поучаствовать
                                 в розыгрыше призов</p>
                         </div>
                     </div>
                 </div>
+                <div className="orangesAll"></div>
             </section>
         )
     }
