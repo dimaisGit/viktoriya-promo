e041655114c68e7623a9d38469fcd391c22f5017
Александра Чернышева <ach@worldfashion.pro>
2019-11-22 18:16:57 +0300

    Updates

---
+++ precache-manifest.49a7663a27c87658d944e68872a810a3.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc813502d9c8fcfaaf23818170462ea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1bbf229869c7637e703",
+    "url": "/static/css/2.4c9116bc.chunk.css"
+  },
+  {
+    "revision": "ac693c70774e682abbf7",
+    "url": "/static/css/main.c2254e8d.chunk.css"
+  },
+  {
+    "revision": "c1bbf229869c7637e703",
+    "url": "/static/js/2.d0ea6917.chunk.js"
+  },
+  {
+    "revision": "ac693c70774e682abbf7",
+    "url": "/static/js/main.0a10c109.chunk.js"
+  },
+  {
+    "revision": "3c343c5daffbc137b225",
+    "url": "/static/js/runtime-main.022fc79d.js"
+  },
+  {
+    "revision": "4bba6aa20e86c60d8c5651b2a8e8ce45",
+    "url": "/static/media/Pomidorko.4bba6aa2.woff2"
+  },
+  {
+    "revision": "6ddd7226f069e761f1b4f40fc2db9c6a",
+    "url": "/static/media/Pomidorko.6ddd7226.ttf"
+  },
+  {
+    "revision": "ad470478173e3224f3e76b291154147f",
+    "url": "/static/media/Pomidorko.ad470478.woff"
+  },
+  {
+    "revision": "345a6e0be0c7efd51dc03bb5a46575e0",
+    "url": "/static/media/arr-next.345a6e0b.png"
+  },
+  {
+    "revision": "75a9a51a9dddd1d21e95933b2e3dcb87",
+    "url": "/static/media/card.75a9a51a.png"
+  },
+  {
+    "revision": "95ded8f86424184e3df50edb743f656f",
+    "url": "/static/media/card.95ded8f8.png"
+  },
+  {
+    "revision": "31bbe7e7920cf77319aed80b5d832fb4",
+    "url": "/static/media/footer-after.31bbe7e7.png"
+  },
+  {
+    "revision": "b78027004797de32c8d4bc7cadff25e4",
+    "url": "/static/media/h1.b7802700.svg"
+  },
+  {
+    "revision": "6249edee3e9a7f206729786810206d2f",
+    "url": "/static/media/leave.6249edee.svg"
+  },
+  {
+    "revision": "38421feb435c7796a27db1e6e5ce25ff",
+    "url": "/static/media/left-brow.38421feb.svg"
+  },
+  {
+    "revision": "830efa5e152fe414187c51f9ce38cb7a",
+    "url": "/static/media/left-eye-closed.830efa5e.svg"
+  },
+  {
+    "revision": "1e6918c697ea3effd38352a541f0e205",
+    "url": "/static/media/left-eye.1e6918c6.svg"
+  },
+  {
+    "revision": "531e69949eb303c48033a16399d6b288",
+    "url": "/static/media/left-hand.531e6994.svg"
+  },
+  {
+    "revision": "c991cfd2afe84d6de8266475c50b992d",
+    "url": "/static/media/list.c991cfd2.png"
+  },
+  {
+    "revision": "b181746bb97b2e9ee5501ce2bb298dcc",
+    "url": "/static/media/logo-wh.b181746b.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "7a482a10bcd2bd7ae29ee5b36becf9fc",
+    "url": "/static/media/mandarin-block-first.7a482a10.png"
+  },
+  {
+    "revision": "20deb544a1b4428f9465478e05dc8f77",
+    "url": "/static/media/mandarin-block-sec.20deb544.png"
+  },
+  {
+    "revision": "12c89c6f7e623982ef6ed4d541a928c0",
+    "url": "/static/media/mandarin-block-third.12c89c6f.png"
+  },
+  {
+    "revision": "0016dba0d0f77b987e5bdbf3cae07b76",
+    "url": "/static/media/mandarin-raffle.0016dba0.svg"
+  },
+  {
+    "revision": "5b9a83cf2877a0d0f79d56cc31c2e508",
+    "url": "/static/media/mandarin.5b9a83cf.svg"
+  },
+  {
+    "revision": "c3b8fb0da51585aa4febc6b695e9460f",
+    "url": "/static/media/mandarin.c3b8fb0d.png"
+  },
+  {
+    "revision": "5a202fbb8c7608e581915d0afea82c8c",
+    "url": "/static/media/map-snow-bg.5a202fbb.png"
+  },
+  {
+    "revision": "872c647951c34d86b20678c546a6afcf",
+    "url": "/static/media/mouth.872c6479.svg"
+  },
+  {
+    "revision": "25ee9a3fab3caaf4a4f48e1a05b56523",
+    "url": "/static/media/oranged-after.25ee9a3f.png"
+  },
+  {
+    "revision": "357ab199e7f31a97aa8fd78af965c5e9",
+    "url": "/static/media/oranged-bg-main-mobile.357ab199.jpg"
+  },
+  {
+    "revision": "7b56a65e99b2fcd15c071203fafcaae8",
+    "url": "/static/media/oranged-bg-main.7b56a65e.jpg"
+  },
+  {
+    "revision": "b45bf55346e6945369dc904d21d3599a",
+    "url": "/static/media/oranged-bg.b45bf553.jpg"
+  },
+  {
+    "revision": "2be4da578d0340a2181ad19ae94f1937",
+    "url": "/static/media/plane.2be4da57.png"
+  },
+  {
+    "revision": "c07d54a0f2b12c0557e0a9904f6329ee",
+    "url": "/static/media/price-box.c07d54a0.png"
+  },
+  {
+    "revision": "1a6bb0513c8bc2a2e1f6a252d41eb89d",
+    "url": "/static/media/raffle-snowflakes-bg.1a6bb051.png"
+  },
+  {
+    "revision": "725be481000e645ec36de8380e7dd059",
+    "url": "/static/media/right-brow.725be481.svg"
+  },
+  {
+    "revision": "46a39494d91ff9dbb0afc737705fa52c",
+    "url": "/static/media/right-eye-closed.46a39494.svg"
+  },
+  {
+    "revision": "b6356b0b4cb7571e089ce341ce1eabf2",
+    "url": "/static/media/right-eye.b6356b0b.svg"
+  },
+  {
+    "revision": "737bef3b860f3b997719326c5fcaa090",
+    "url": "/static/media/snowflakes-bg-mechanics.737bef3b.png"
+  },
+  {
+    "revision": "a8728306033824825c6c2a887fdda3cc",
+    "url": "/static/media/snowflakes-bg-mobile.a8728306.png"
+  },
+  {
+    "revision": "a3d2a0781dc5d37804e48d86d38827a1",
+    "url": "/static/media/snowflakes-bg-prices.a3d2a078.png"
+  },
+  {
+    "revision": "3cb15569837f13f160ae62e045551dd4",
+    "url": "/static/media/snowflakes-bg.3cb15569.png"
+  },
+  {
+    "revision": "84f660f1f498150cba78574afe1c4dee",
+    "url": "/static/media/tail.84f660f1.svg"
+  },
+  {
+    "revision": "2803f44f8506c5d55d38521249d67706",
+    "url": "/static/media/tel.2803f44f.png"
+  },
+  {
+    "revision": "d351b9adb9d29dc85b66bb797d71aabc",
+    "url": "/static/media/tel.d351b9ad.png"
+  },
+  {
+    "revision": "800072c2416a08be16daf3d7fd158ed9",
+    "url": "/static/media/tour.800072c2.png"
+  },
+  {
+    "revision": "f3b525aec2327f756bcc4bb0ca1e4303",
+    "url": "/static/media/tour.f3b525ae.png"
+  },
+  {
+    "revision": "591d6818534f2173e422cae762e4630c",
+    "url": "/static/media/trees-bg.591d6818.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manda-react/precache-manifest.9f87a429d1a47fff1db8d67183d0cb02.js"
+  "/precache-manifest.49a7663a27c87658d944e68872a810a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manda-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
