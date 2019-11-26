75dadbd00e0049d9b71c817337c21b331f9bc13c
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-11-26 14:03:52 +0300

    Updates

---
+++ precache-manifest.3d6229b265ad977f6943cba4e014b7c4.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3828e97804c9e0491fabc822a48f00d2",
+    "url": "/manda-react/index.html"
+  },
+  {
+    "revision": "4d9b005bcea7ff6ed4f6",
+    "url": "/manda-react/static/css/2.4c9116bc.chunk.css"
+  },
+  {
+    "revision": "84c5806fbf913c7479de",
+    "url": "/manda-react/static/css/main.7cd4a8b2.chunk.css"
+  },
+  {
+    "revision": "4d9b005bcea7ff6ed4f6",
+    "url": "/manda-react/static/js/2.14224cbd.chunk.js"
+  },
+  {
+    "revision": "84c5806fbf913c7479de",
+    "url": "/manda-react/static/js/main.9288748d.chunk.js"
+  },
+  {
+    "revision": "94f005a0036619d036aa",
+    "url": "/manda-react/static/js/runtime-main.48f88ba5.js"
+  },
+  {
+    "revision": "4bba6aa20e86c60d8c5651b2a8e8ce45",
+    "url": "/manda-react/static/media/Pomidorko.4bba6aa2.woff2"
+  },
+  {
+    "revision": "6ddd7226f069e761f1b4f40fc2db9c6a",
+    "url": "/manda-react/static/media/Pomidorko.6ddd7226.ttf"
+  },
+  {
+    "revision": "ad470478173e3224f3e76b291154147f",
+    "url": "/manda-react/static/media/Pomidorko.ad470478.woff"
+  },
+  {
+    "revision": "345a6e0be0c7efd51dc03bb5a46575e0",
+    "url": "/manda-react/static/media/arr-next.345a6e0b.png"
+  },
+  {
+    "revision": "5dfdbfbe6c9e8aaf629516511095fa11",
+    "url": "/manda-react/static/media/card.5dfdbfbe.png"
+  },
+  {
+    "revision": "95ded8f86424184e3df50edb743f656f",
+    "url": "/manda-react/static/media/card.95ded8f8.png"
+  },
+  {
+    "revision": "8222cbd951e10f0ef6e92faf1465881e",
+    "url": "/manda-react/static/media/fir-2.8222cbd9.png"
+  },
+  {
+    "revision": "a25bd5835fc9dcafdf85083cb3151747",
+    "url": "/manda-react/static/media/fir.a25bd583.png"
+  },
+  {
+    "revision": "31bbe7e7920cf77319aed80b5d832fb4",
+    "url": "/manda-react/static/media/footer-after.31bbe7e7.png"
+  },
+  {
+    "revision": "b78027004797de32c8d4bc7cadff25e4",
+    "url": "/manda-react/static/media/h1.b7802700.svg"
+  },
+  {
+    "revision": "6249edee3e9a7f206729786810206d2f",
+    "url": "/manda-react/static/media/leave.6249edee.svg"
+  },
+  {
+    "revision": "38421feb435c7796a27db1e6e5ce25ff",
+    "url": "/manda-react/static/media/left-brow.38421feb.svg"
+  },
+  {
+    "revision": "830efa5e152fe414187c51f9ce38cb7a",
+    "url": "/manda-react/static/media/left-eye-closed.830efa5e.svg"
+  },
+  {
+    "revision": "1e6918c697ea3effd38352a541f0e205",
+    "url": "/manda-react/static/media/left-eye.1e6918c6.svg"
+  },
+  {
+    "revision": "531e69949eb303c48033a16399d6b288",
+    "url": "/manda-react/static/media/left-hand.531e6994.svg"
+  },
+  {
+    "revision": "c991cfd2afe84d6de8266475c50b992d",
+    "url": "/manda-react/static/media/list.c991cfd2.png"
+  },
+  {
+    "revision": "b181746bb97b2e9ee5501ce2bb298dcc",
+    "url": "/manda-react/static/media/logo-wh.b181746b.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/manda-react/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d217ab676b218cfc5586663eead6f682",
+    "url": "/manda-react/static/media/mandar.d217ab67.gif"
+  },
+  {
+    "revision": "7a482a10bcd2bd7ae29ee5b36becf9fc",
+    "url": "/manda-react/static/media/mandarin-block-first.7a482a10.png"
+  },
+  {
+    "revision": "20deb544a1b4428f9465478e05dc8f77",
+    "url": "/manda-react/static/media/mandarin-block-sec.20deb544.png"
+  },
+  {
+    "revision": "0016dba0d0f77b987e5bdbf3cae07b76",
+    "url": "/manda-react/static/media/mandarin-raffle.0016dba0.svg"
+  },
+  {
+    "revision": "5b9a83cf2877a0d0f79d56cc31c2e508",
+    "url": "/manda-react/static/media/mandarin.5b9a83cf.svg"
+  },
+  {
+    "revision": "c3b8fb0da51585aa4febc6b695e9460f",
+    "url": "/manda-react/static/media/mandarin.c3b8fb0d.png"
+  },
+  {
+    "revision": "5a202fbb8c7608e581915d0afea82c8c",
+    "url": "/manda-react/static/media/map-snow-bg.5a202fbb.png"
+  },
+  {
+    "revision": "872c647951c34d86b20678c546a6afcf",
+    "url": "/manda-react/static/media/mouth.872c6479.svg"
+  },
+  {
+    "revision": "25ee9a3fab3caaf4a4f48e1a05b56523",
+    "url": "/manda-react/static/media/oranged-after-old.25ee9a3f.png"
+  },
+  {
+    "revision": "1d94658977c645f6159941f4610f61f2",
+    "url": "/manda-react/static/media/oranged-after.1d946589.png"
+  },
+  {
+    "revision": "f12fbda5d3f1c832bbee18d8b9b1a874",
+    "url": "/manda-react/static/media/oranged-bg-main-mobile-l.f12fbda5.png"
+  },
+  {
+    "revision": "357ab199e7f31a97aa8fd78af965c5e9",
+    "url": "/manda-react/static/media/oranged-bg-main-mobile.357ab199.jpg"
+  },
+  {
+    "revision": "7b56a65e99b2fcd15c071203fafcaae8",
+    "url": "/manda-react/static/media/oranged-bg-main.7b56a65e.jpg"
+  },
+  {
+    "revision": "b45bf55346e6945369dc904d21d3599a",
+    "url": "/manda-react/static/media/oranged-bg.b45bf553.jpg"
+  },
+  {
+    "revision": "2be4da578d0340a2181ad19ae94f1937",
+    "url": "/manda-react/static/media/plane.2be4da57.png"
+  },
+  {
+    "revision": "c07d54a0f2b12c0557e0a9904f6329ee",
+    "url": "/manda-react/static/media/price-box.c07d54a0.png"
+  },
+  {
+    "revision": "1a6bb0513c8bc2a2e1f6a252d41eb89d",
+    "url": "/manda-react/static/media/raffle-snowflakes-bg.1a6bb051.png"
+  },
+  {
+    "revision": "725be481000e645ec36de8380e7dd059",
+    "url": "/manda-react/static/media/right-brow.725be481.svg"
+  },
+  {
+    "revision": "46a39494d91ff9dbb0afc737705fa52c",
+    "url": "/manda-react/static/media/right-eye-closed.46a39494.svg"
+  },
+  {
+    "revision": "b6356b0b4cb7571e089ce341ce1eabf2",
+    "url": "/manda-react/static/media/right-eye.b6356b0b.svg"
+  },
+  {
+    "revision": "c61e2fb65b80402ba7b24e8158edafeb",
+    "url": "/manda-react/static/media/snow-after.c61e2fb6.png"
+  },
+  {
+    "revision": "737bef3b860f3b997719326c5fcaa090",
+    "url": "/manda-react/static/media/snowflakes-bg-mechanics.737bef3b.png"
+  },
+  {
+    "revision": "a8728306033824825c6c2a887fdda3cc",
+    "url": "/manda-react/static/media/snowflakes-bg-mobile.a8728306.png"
+  },
+  {
+    "revision": "a3d2a0781dc5d37804e48d86d38827a1",
+    "url": "/manda-react/static/media/snowflakes-bg-prices.a3d2a078.png"
+  },
+  {
+    "revision": "3cb15569837f13f160ae62e045551dd4",
+    "url": "/manda-react/static/media/snowflakes-bg.3cb15569.png"
+  },
+  {
+    "revision": "84f660f1f498150cba78574afe1c4dee",
+    "url": "/manda-react/static/media/tail.84f660f1.svg"
+  },
+  {
+    "revision": "363d4426b4c24e7b2de0898697e91d7a",
+    "url": "/manda-react/static/media/tel.363d4426.png"
+  },
+  {
+    "revision": "d351b9adb9d29dc85b66bb797d71aabc",
+    "url": "/manda-react/static/media/tel.d351b9ad.png"
+  },
+  {
+    "revision": "107978d4038b86c925e2a25f813374d8",
+    "url": "/manda-react/static/media/tour.107978d4.png"
+  },
+  {
+    "revision": "f3b525aec2327f756bcc4bb0ca1e4303",
+    "url": "/manda-react/static/media/tour.f3b525ae.png"
+  },
+  {
+    "revision": "9d27d08e45774de8061250edad629688",
+    "url": "/manda-react/static/media/trees-bg-mob.9d27d08e.png"
+  },
+  {
+    "revision": "591d6818534f2173e422cae762e4630c",
+    "url": "/manda-react/static/media/trees-bg.591d6818.svg"
+  }
+]);
