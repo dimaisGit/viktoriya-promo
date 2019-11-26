259c127d14dc835f2bb36bbac0d361356838cedc
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-11-26 13:42:00 +0300

    mega commit

--- src/components/Mechanics.js
+++ src/components/Mechanics.js
@@ -18,38 +18,41 @@ export default class MechanicsComponent extends React.Component {
                         <div className="block first">
                             <p>С <b>16.12.2019</b> ПО <b>31.01.2020</b> СОВЕРШИТЕ ПОКУПКУ
                                 НА СУММУ ОТ 700Р*</p>
-                <div className="ramka">
-                    <div className="container">
-                        <h2>Встречай Новый год вместе с нами!</h2>
-                        <div className="blocks flexing">
-                            <div className="block first">
-                                <p>Соверши покупку ОТ 700<small>₽</small> и получи купон (скретч-карту)</p>
-                        </div>
-                            <div className="block second">
-                                <p>Под защитным слоем ты найдешь один из подарков:</p>
-                            </div>
-                        </div>
-                        <div className="blocks flexing">
-                            <div className="block no-bg">
-                                <h4>скидку до 20%</h4>
-                                <p>На следующую покупку от 700<small>₽</small> в <b>«ДИКСИ»</b>
-                                    <Tooltip
-                                        title="Без учета табака, табачной продукции, табачных изделий, курительных принадлежностей, алкогольной продукции с установленной минимальной розничной ценой"
-                                        position="top"
-                                        trigger="click" >
-                                        <img className="tooltip-min" src={toolt} />
-                                    </Tooltip>
-                                </p>
-                            </div>
-                            <div className="block no-bg">
-                                <h4>код «2020»</h4>
-                                <p>Для участия в новогоднем розыгрыше <b>СУПЕРПРИЗОВ</b>!</p>
-                            </div>
                         </div>
                     </div>
-                    <div className="orangesAll">
-                        <img src={mandarin} />
-                        {/* <Mandarin /> */}
+                    <div className="ramka">
+                        <div className="container">
+                            <h2>Встречай Новый год вместе с нами!</h2>
+                            <div className="blocks flexing">
+                                <div className="block first">
+                                    <p>Соверши покупку ОТ 700<small>₽</small> и получи купон (скретч-карту)</p>
+                            </div>
+                                <div className="block second">
+                                    <p>Под защитным слоем ты найдешь один из подарков:</p>
+                                </div>
+                            </div>
+                            <div className="blocks flexing">
+                                <div className="block no-bg">
+                                    <h4>скидку до 20%</h4>
+                                    <p>На следующую покупку от 700<small>₽</small> в <b>«ДИКСИ»</b>
+                                        <Tooltip
+                                            title="Без учета табака, табачной продукции, табачных изделий, курительных принадлежностей, алкогольной продукции с установленной минимальной розничной ценой"
+                                            position="top"
+                                            trigger="click" >
+                                            <img className="tooltip-min" src={toolt} />
+                                        </Tooltip>
+                                    </p>
+                                </div>
+                                <div className="block no-bg">
+                                    <h4>код «2020»</h4>
+                                    <p>Для участия в новогоднем розыгрыше <b>СУПЕРПРИЗОВ</b>!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="orangesAll">
+                            <img src={mandarin} />
+                            {/* <Mandarin /> */}
+                        </div>
                     </div>
                 </div>
             </section>
