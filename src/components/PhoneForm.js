4b2ab160ec8e5ce0bac58ec73fa43209fe7e942e
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-12-03 20:13:40 +0300

    mega commit

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Field, reduxForm, SubmissionError } from "redux-form";
+import { Field, reduxForm, SubmissionError, change } from "redux-form";
 import { RenderField } from "./RenderField";
 
 
@@ -7,20 +7,16 @@ import { RenderField } from "./RenderField";
 class PhoneForm extends React.Component {
 
     onHandleSubmit = values => {
-        if (!values.phoneRegion){
-            throw new SubmissionError({ userPhone: 'Phone code is required', _error: "Login failed"})
-        }
         if (!values.userPhone) {
             throw new SubmissionError({ userPhone: 'Phone is required', _error: 'Login failed!' })
         }
         if (this.props.codeSent && !values.userCode) {
             throw new SubmissionError({ userCode: 'Code is required', _error: 'Login failed!'})
         }
-        console.log(values.phoneRegion + values.userPhone)
         if (!this.props.codeSent)
-            this.props.handleSendCode(values.userPhone)
+            this.props.handleSendCode('+7' + values.userPhone)
         else
-            this.props.handleCheckCode(values.userPhone, values.userCode)
+            this.props.handleCheckCode('+7' + values.userPhone, values.userCode)
     }
 
     render() {
@@ -29,12 +25,6 @@ class PhoneForm extends React.Component {
             <form onSubmit={handleSubmit(this.onHandleSubmit)}>
                 <h2>Введите номер телефона</h2>
                 <div className="cancelBut"></div>
-                {/* <Field name="phoneRegion" component="select" className="numberPlus">
-                    <option></option>
-                    <option value="+7" selected>+7</option>
-                    <option value="+380">+380</option>
-                    <option value="+375">+375</option>
-                </Field> */}
                 <Field name="userPhone" component={RenderField} placeholder=''/>
                 <div>
                     <button type="submit" disabled={submitting}>Отправить код</button>
